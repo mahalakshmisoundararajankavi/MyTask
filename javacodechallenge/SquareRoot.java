ff6a36a809057d1cbbe4a8b7b9558486dc291f97
Mahalakshmi S <98319446+mahalakshmisoundararajankavi@users.noreply.github.com>
2023-05-03 18:54:52 +0530

    MyTask

---
+++ javacodechallenge/SquareRoot.java
@@ -0,0 +1,12 @@
+package javacodechallenge;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		int input = 4;
+		int result = (int) Math.sqrt(input);
+		System.out.println(result);
+
+	}
+
+}
