507257f519359ecf9039edd3f9685924cb4b6028
Mahalakshmi S <98319446+mahalakshmisoundararajankavi@users.noreply.github.com>
2023-05-04 11:52:51 +0530

    Add files via upload

--- javacodechallenge/SquareRoot.java
+++ javacodechallenge/SquareRoot.java
@@ -4,9 +4,17 @@ public class SquareRoot {
 
 	public static void main(String[] args) {
 		int input = 4;
-		int result = (int) Math.sqrt(input);
-		System.out.println(result);
-
+		int squareRoot = input/2;
+		for (int i = 1; i < input; i++) {
+			squareRoot = squareRoot-(squareRoot*squareRoot-input)/(2*squareRoot);
+		}
+		int i =1;
+		if(i*i == input) {
+			System.out.println("Square Root : " +squareRoot);
+		}
+		else {
+			System.out.println("Not Square Root : " +squareRoot);
+		}
 	}
 
 }
