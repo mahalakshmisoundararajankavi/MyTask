bfd5bea0aeba65f573ce71235382f11ce7e15e25
Mahalakshmi S <98319446+mahalakshmisoundararajankavi@users.noreply.github.com>
2023-05-04 13:29:35 +0530

    Add files via upload

--- javacodechallenge/PalindromeChar.java
+++ javacodechallenge/PalindromeChar.java
@@ -1,19 +1,18 @@
 package javacodechallenge;
 
-public class Palindrome {
+public class PalindromeChar {
 
 	public static void main(String[] args) {
-		String textInput = "A man, a plan, a canal: Panama";
-		boolean isPalindrome = true;
+		String textInput = "race a car";
+		boolean notPalindrome = true;
 		String result = textInput.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 	    for (int i = 0; i < result.length()-1; i++) {
 			if(result.charAt(i) != result.charAt(result.length()-1)) {
-				// System.out.println(result);
-				isPalindrome = true;
+				notPalindrome = false;
 			}
 		}
-	    System.out.println(isPalindrome);
-	   System.out.println(result + " " + " is a palindrome");
+	    System.out.println(notPalindrome);
+	   System.out.println(result + " " + " is a Not palindrome");
 	}
 
 }
